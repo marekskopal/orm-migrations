e62c1c9d7bfccb686dc77ea9a680258f7c2b4cda
Marek Skopal <skopal.marek@gmail.com>
2026-03-09 00:55:53 +0100

    Add OneToMany and ManyToMany relations

--- src/Schema/Converter/OrmSchemaConverter.php
+++ src/Schema/Converter/OrmSchemaConverter.php
@@ -23,7 +23,12 @@ class OrmSchemaConverter
             $columns = [];
 
             foreach ($entity->columns as $column) {
-                if ($column->relationType === RelationEnum::OneToMany) {
+                if (in_array($column->relationType, [
+                    RelationEnum::OneToMany,
+                    RelationEnum::OneToOneInverse,
+                    RelationEnum::ManyToMany,
+                    RelationEnum::ManyToManyInverse,
+                ], true)) {
                     continue;
                 }
 
@@ -46,7 +51,12 @@ class OrmSchemaConverter
             $foreignKeys = [];
 
             foreach ($entity->columns as $column) {
-                if ($column->relationType === null || $column->relationType === RelationEnum::OneToMany) {
+                if ($column->relationType === null || in_array($column->relationType, [
+                    RelationEnum::OneToMany,
+                    RelationEnum::OneToOneInverse,
+                    RelationEnum::ManyToMany,
+                    RelationEnum::ManyToManyInverse,
+                ], true)) {
                     continue;
                 }
 
@@ -60,7 +70,7 @@ class OrmSchemaConverter
                 $indexes[$column->columnName] = new IndexSchema(
                     columns: [$column->columnName],
                     name: implode('_', [$entity->table, $column->columnName, 'index']),
-                    unique: false,
+                    unique: $column->relationType === RelationEnum::OneToOne,
                 );
 
                 $foreignKeys[$column->columnName] = new ForeignKeySchema(
@@ -75,6 +85,87 @@ class OrmSchemaConverter
             }
 
             $tables[$entity->table] = new TableSchema($entity->table, $columns, $indexes, $foreignKeys);
+
+            foreach ($entity->columns as $column) {
+                if ($column->relationType !== RelationEnum::ManyToMany) {
+                    continue;
+                }
+
+                /** @phpstan-ignore-next-line offsetAccess.notFound */
+                $relationEntitySchema = $schema->entities[$column->relationEntityClass];
+                $joinTable = $column->joinTable ?? throw new \RuntimeException(
+                    sprintf('ManyToMany column "%s" is missing joinTable', $column->columnName),
+                );
+                $joinColumn = $column->joinColumn ?? throw new \RuntimeException(
+                    sprintf('ManyToMany column "%s" is missing joinColumn', $column->columnName),
+                );
+                $inverseJoinColumn = $column->inverseJoinColumn ?? throw new \RuntimeException(
+                    sprintf('ManyToMany column "%s" is missing inverseJoinColumn', $column->columnName),
+                );
+
+                $joinColumns = [
+                    $joinColumn => new ColumnSchema(
+                        name: $joinColumn,
+                        type: $entity->getPrimaryColumn()->columnType,
+                        nullable: false,
+                        autoincrement: false,
+                        primary: false,
+                        size: null,
+                        precision: null,
+                        scale: null,
+                        enum: null,
+                        default: null,
+                    ),
+                    $inverseJoinColumn => new ColumnSchema(
+                        name: $inverseJoinColumn,
+                        type: $relationEntitySchema->getPrimaryColumn()->columnType,
+                        nullable: false,
+                        autoincrement: false,
+                        primary: false,
+                        size: null,
+                        precision: null,
+                        scale: null,
+                        enum: null,
+                        default: null,
+                    ),
+                ];
+
+                $entityPrimaryColumn = $entity->getPrimaryColumn()->columnName;
+                $relationPrimaryColumn = $relationEntitySchema->getPrimaryColumn()->columnName;
+
+                $joinIndexes = [
+                    $joinColumn => new IndexSchema(
+                        columns: [$joinColumn],
+                        name: implode('_', [$joinTable, $joinColumn, 'index']),
+                        unique: false,
+                    ),
+                    $inverseJoinColumn => new IndexSchema(
+                        columns: [$inverseJoinColumn],
+                        name: implode('_', [$joinTable, $inverseJoinColumn, 'index']),
+                        unique: false,
+                    ),
+                ];
+
+                $joinForeignKeys = [
+                    $joinColumn => new ForeignKeySchema(
+                        column: $joinColumn,
+                        referenceTable: $entity->table,
+                        referenceColumn: $entityPrimaryColumn,
+                        name: implode('_', [$joinTable, $joinColumn, $entity->table, $entityPrimaryColumn, 'fk']),
+                    ),
+                    $inverseJoinColumn => new ForeignKeySchema(
+                        column: $inverseJoinColumn,
+                        referenceTable: $relationEntitySchema->table,
+                        referenceColumn: $relationPrimaryColumn,
+                        name: implode(
+                            '_',
+                            [$joinTable, $inverseJoinColumn, $relationEntitySchema->table, $relationPrimaryColumn, 'fk'],
+                        ),
+                    ),
+                ];
+
+                $tables[$joinTable] = new TableSchema($joinTable, $joinColumns, $joinIndexes, $joinForeignKeys);
+            }
         }
 
         return new DatabaseSchema($tables);
